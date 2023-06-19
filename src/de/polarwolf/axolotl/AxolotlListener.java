0eeb28278c8288b7e7b0a18abbe71950b0ee39de
Kirastur <76752138+Kirastur@users.noreply.github.com>
2023-06-19 20:31:13 +0200

    Version 2.0

--- src/de/polarwolf/axolotl/AxolotlListener.java
+++ src/de/polarwolf/axolotl/AxolotlListener.java
@@ -1,5 +1,6 @@
 package de.polarwolf.axolotl;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Axolotl;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
@@ -7,20 +8,14 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 
-import de.polarwolf.heliumballoon.config.ConfigSection;
 import de.polarwolf.heliumballoon.elements.Element;
-import de.polarwolf.heliumballoon.elements.LivingElement;
+import de.polarwolf.heliumballoon.elements.living.LivingElement;
 import de.polarwolf.heliumballoon.events.BalloonElementCreateEvent;
-import de.polarwolf.heliumballoon.events.BalloonRebuildConfigEvent;
-import de.polarwolf.heliumballoon.exception.BalloonException;
 
 public class AxolotlListener implements Listener {
 
-	protected final Plugin plugin;
-
 	public AxolotlListener(Plugin plugin) {
-		this.plugin = plugin;
-		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		Bukkit.getPluginManager().registerEvents(this, plugin);
 	}
 
 	// This event is called every time when the Minecraft entity
@@ -81,35 +76,6 @@ public class AxolotlListener implements Listener {
 			// If something goes wrong here
 			// simply print the exception to the server console.
 			e.printStackTrace();
-		}
-	}
-
-	// This event reloads our custom config
-	// from our own config.yml
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void onBalloonRebuildConfigEvent(BalloonRebuildConfigEvent event) {
-		try {
-
-			// Don't load our config if the Rebuild is already cancelled
-			if (event.isCancelled()) {
-				return;
-			}
-
-			// reload our config.yml
-			plugin.reloadConfig();
-
-			// Build our Balloon ConfigSection using the standard syntax
-			ConfigSection newSection = event.buildConfigSectionFromConfigFile(plugin);
-
-			// Add my new ConfigSection to the list of valid sections
-			event.addSection(newSection);
-
-		} catch (BalloonException be) {
-			be.printStackTrace();
-			event.cancelWithReason(be);
-		} catch (Exception e) {
-			e.printStackTrace();
-			event.cancelWithReason(new BalloonException(null, BalloonException.JAVA_EXCEPTION, null, e));
 		}
 	}
 
--- src/de/polarwolf/axolotl/Main.java
+++ src/de/polarwolf/axolotl/Main.java
@@ -2,11 +2,14 @@ package de.polarwolf.axolotl;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import de.polarwolf.heliumballoon.api.HeliumBalloonProvider;
+
 public final class Main extends JavaPlugin {
-	
+
 	@Override
 	public void onEnable() {
 		saveDefaultConfig();
+		HeliumBalloonProvider.getAPI().addReloadRegistration(this, null, null);
 		new AxolotlListener(this);
 		this.getLogger().info("You can now use customized axolotls as your HeliumBalloon pet.");
 	}
